3fea27287ba5f892715f37eb99489b7aca64bd73
Paige Burlew <paigeburlew@gmail.com>
2020-09-28 23:04:34 -0500

    js for entrytextbox functional

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
+// define const
 const entryForm = document.getElementById('entryForm');
+const entriesSection = document.getElementById('entries');
+const entryTextbox = document.getElementsByClassName('entry-textbox');
 
-console.log(entryForm);
+
+
+
+
 
 function addEntryToDom(event){
     event.preventDefault();
     // this prevents the page from refreshing every time we enter text
+    console.log(entryTextbox[0].value);
 }
 
 entryForm.addEventListener('submit', addEntryToDom)
