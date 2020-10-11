9469398d714166076d13713ca3848314037de23f
Paige Burlew <paigeburlew@gmail.com>
2020-10-11 12:50:59 -0500

    css and js button count increment

--- index.js
+++ index.js
@@ -1,22 +1,35 @@
 // define const
 const entryForm = document.getElementById('entryForm');
-const entriesSection = document.getElementById('entries');
-const entryTextbox = document.getElementsByClassName('entry-textbox');
+const entriesSection = document.querySelector('#entries');
+const entryTextbox = document.querySelector('.entry-textbox');
+const entriesNav = document.querySelector('.entries-nav');
 
 
 
 
 
-
+let count = 1;
 function addEntryToDom(event){
     event.preventDefault();
     // this prevents the page from refreshing every time we enter text
     const entryDiv = document.createElement('div');
-    // console.log(entryDiv)
-//     console.log(entryTextbox[0].value);
+   
     entryDiv.className = 'single-entry';
-    entryDiv.innerText = entryTextbox[0].value;
+    entryDiv.innerText = entryTextbox.value;
+    entryDiv.style.display = 'none';
+    
     entriesSection.appendChild(entryDiv);
+
+    //clear the entry textbox whenever the submit button is clicked
+    entryTextbox.value = '';
+
+    const displayEntryButton = document.createElement('button');
+
+    displayEntryButton.className = 'display-entry-button';
+    displayEntryButton.innerText = count;
+    entriesNav.appendChild(displayEntryButton);
+
+    count++;
 }
 
 entryForm.addEventListener('submit', addEntryToDom)
