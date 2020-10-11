4336470c9b78d06cf3fd45f28bc47f542655c2c6
Paige Burlew <paigeburlew@gmail.com>
2020-10-11 12:33:20 -0500

    entried section js working

--- index.js
+++ index.js
@@ -11,7 +11,12 @@ const entryTextbox = document.getElementsByClassName('entry-textbox');
 function addEntryToDom(event){
     event.preventDefault();
     // this prevents the page from refreshing every time we enter text
-    console.log(entryTextbox[0].value);
+    const entryDiv = document.createElement('div');
+    // console.log(entryDiv)
+//     console.log(entryTextbox[0].value);
+    entryDiv.className = 'single-entry';
+    entryDiv.innerText = entryTextbox[0].value;
+    entriesSection.appendChild(entryDiv);
 }
 
 entryForm.addEventListener('submit', addEntryToDom)
